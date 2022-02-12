d8ab237feaa2f89f779639b17966e4d7d6073b23
jiheddachraoui <jiheddachraoui@outlook.fr>
2022-02-13 00:06:54 +0100

    pager

---
+++ services/src/main/java/com/example/services/utility/pager.java
@@ -0,0 +1,41 @@
+package com.example.services.utility;
+import com.example.services.model.Product;
+import org.springframework.data.domain.Page;
+
+public class pager {
+
+    private final Page<Product> products;
+
+    public pager(Page<Product> products) {
+        this.products = products;
+    }
+
+    public int getPageIndex() {
+        return products.getNumber() + 1;
+    }
+
+    public int getPageSize() {
+        return products.getSize();
+    }
+
+    public boolean hasNext() {
+        return products.hasNext();
+    }
+
+    public boolean hasPrevious() {
+        return products.hasPrevious();
+    }
+
+    public int getTotalPages() {
+        return products.getTotalPages();
+    }
+
+    public long getTotalElements() {
+        return products.getTotalElements();
+    }
+
+    public boolean indexOutOfBounds() {
+        return this.getPageIndex() < 0 || this.getPageIndex() > this.getTotalElements();
+    }
+
+}
